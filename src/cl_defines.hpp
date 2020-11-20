480d35efb5a8516326502103a83d55a5aa36b298
mkarpenko <mkarpenko.spb@mail.ru>
2020-11-20 21:08:59 +0300

    add matrix_coo

---
+++ src/cl_defines.hpp
@@ -0,0 +1,8 @@
+#pragma once
+
+#define CL_HPP_ENABLE_EXCEPTIONS
+#define CL_HPP_MINIMUM_OPENCL_VERSION 110
+#define CL_HPP_TARGET_OPENCL_VERSION 110
+
+#include "../libs/clew/CL/cl.h"
+#include "../libs/clew/CL/cl2.hpp"
