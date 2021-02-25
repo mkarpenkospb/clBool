a84a3b265dbb9ef1d60524eda226e9dcb957f8de
root <mkarpenko.spb@mail.ru>
2021-02-26 02:25:28 +0300

    [from server]

--- reports/lib/product_data.js
+++ reports/lib/product_data.js
@@ -1,1 +1,1 @@
-var infoJSON={"compileInfo":{"nodes":[{"command":"aoc simple_addition_branch.cl","family":"Arria 10, 10AX115S2F45E2LG, euler2:line_high","id":1,"name":"simple_addition_branch","product":"OPENCL","quartus":"20.4.0 Build 72 Pro","time":"Thu Feb 25 01:07:10 2021","version":"20.4.0 Build 72"}]}};
+var infoJSON={"compileInfo":{"nodes":[{"command":"aoc simple_addition.cl","family":"Arria 10, 10AX115S2F45I1SG, a10_ref:a10gx","id":1,"name":"simple_addition","product":"OPENCL","quartus":"20.4.0 Build 72 Pro","time":"Wed Feb 24 22:30:58 2021","version":"20.4.0 Build 72"}]}};
