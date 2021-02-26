dfb8363b4e5c4b9a23d61a440e16288c7d964ab7
Maria Karpenko <mkarpenko.spb@mail.ru>
2021-02-27 01:09:41 +0300

    [to server]

--- src/common/cl_operations.cpp
+++ src/common/cl_operations.cpp
@@ -10,17 +10,17 @@ void prefix_sum(Controls &controls,
                 uint32_t array_size) {
     cl::Program program;
     try {
-#ifndef FPGA
-        program = controls.create_program_from_source(prefix_sum_kernel, prefix_sum_kernel_length);
-#else
+#ifdef FPGA
         program = controls.create_program_from_binaries("prefix_sum");
-#endif
-        uint32_t block_size = controls.block_size;
-
+        program.build();
+#else
+        program = controls.create_program_from_source(prefix_sum_kernel, prefix_sum_kernel_length);
         std::stringstream options;
         options << "-D RUN " << "-D GROUP_SIZE=" << block_size;
 
         program.build(options.str().c_str());
+#endif
+        uint32_t block_size = controls.block_size;
 
         uint32_t work_group_size = block_size;
         uint32_t global_work_size = utils::calculate_global_size(work_group_size, array_size);
