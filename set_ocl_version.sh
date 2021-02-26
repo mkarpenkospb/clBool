48e2084ef9167246e4b99ce314d54f3be608d2a9
Maria Karpenko <mkarpenko.spb@mail.ru>
2021-02-26 14:00:40 +0300

    [to server] env SDK

--- set_ocl_version.sh
+++ set_ocl_version.sh
@@ -2,12 +2,12 @@
 SDK_VERSION=$1
 QARTUS_VERSION=$2
 
-export ALTERAOCLSDKROOT=/opt/intelFPGA_pro/SDK_VERSION/hld # hmm
+export ALTERAOCLSDKROOT=/opt/intelFPGA_pro/$SDK_VERSION/hld # hmm
 export SOCEDS_ROOT=/opt/intelFPGA_pro/17.1/embedded # hmm not existing directory
-export QSYS_ROOTDIR=/opt/intelFPGA_pro/SDK_VERSION/qsys/bin
-export LD_LIBRARY_PATH=/opt/intelFPGA_pro/SDK_VERSION/hld/host/linux64/lib:
-export INTELFPGAOCLSDKROOT=/opt/intelFPGA_pro/SDK_VERSION/hld
-export PATH=/opt/intelFPGA_pro/SDK_VERSION/hld/bin:/opt/intelFPGA_pro/QARTUS_VERSION/quartus/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/snap/bin
-export QUARTUS_ROOTDIR_OVERRIDE=/opt/intelFPGA_pro/QARTUS_VERSION/quartus
+export QSYS_ROOTDIR=/opt/intelFPGA_pro/$SDK_VERSION/qsys/bin
+export LD_LIBRARY_PATH=/opt/intelFPGA_pro/$SDK_VERSION/hld/host/linux64/lib:
+export INTELFPGAOCLSDKROOT=/opt/intelFPGA_pro/$SDK_VERSION/hld
+export PATH=/opt/intelFPGA_pro/$SDK_VERSION/hld/bin:/opt/intelFPGA_pro/$QARTUS_VERSION/quartus/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/snap/bin
+export QUARTUS_ROOTDIR_OVERRIDE=/opt/intelFPGA_pro/$QARTUS_VERSION/quartus
 # bsp always 17
 export AOCL_BOARD_PACKAGE_ROOT=/opt/intelFPGA_pro/17.1/hld/board/euler2
