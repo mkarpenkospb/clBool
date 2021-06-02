988b0e0c2ca0a967e70d121201670faddd0a0b62
Maria Karpenko <mkarpenko.spb@mail.ru>
2021-06-02 16:43:35 +0300

    [to server] modification: counter

--- test/test_operations.cpp
+++ test/test_operations.cpp
@@ -70,7 +70,7 @@ TEST(clBool_operations, addition_coo) {
 
 TEST(clBool_operations, addition_csr) {
     clbool::Controls controls = clbool::create_controls();
-    for (int size = 0; size < 20000; size += 200) {
+    for (int size = 0; size < 10000; size += 200) {
         for (uint32_t k_a = 20; k_a < 40; k_a += 5) {
             for (uint32_t k_b = 20; k_b < 40; k_b += 5) {
                 ASSERT_TRUE(test_addition_csr(controls, size, k_a, k_b));
